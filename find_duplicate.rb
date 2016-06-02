255f2f5e2cd8c5ce1bf4325673c9bfaa7e5e5d30
maycmlee <may.cm.lee@gmail.com>
2016-06-02 14:43:04 -0700

    add alternative solutions

--- find_duplicate.rb
+++ find_duplicate.rb
@@ -1,7 +1,24 @@
 # Given an array that contains numbers from 1 to n-1
 # and exactly 1 duplicate, find that duplicate.
 
-def find_duplicate(array)
+# Run time...O(n*(n+1)/2)
+def find_duplicate_one(array)
+  i = 0
+  while i < array.length
+    j = i + 1
+
+    while j < array.length
+      if array[i] == array[j]
+        return array[i]
+      end
+      j += 1
+    end
+    i += 1
+  end
+end
+
+# Run time..O(n)
+def find_duplicate_two(array)
   array_sum = 0
   i = 0
   while i < array.length
@@ -16,15 +33,30 @@ def find_duplicate(array)
     sum += i
     i += 1
   end
-
   duplicate = array_sum - sum
-
   return duplicate
-
 end
 
-answer = find_duplicate([2, 5, 1, 5, 4, 3])
+# Sort-in-place solution.
+# Run time...O(n)
+def find_duplicate_three(array)
+  count = 0
+  i = array.length - 1
+  while count < array.length
+    if array[i] != i
+      temp = array[i]
+      i = temp
+      count += 1
+    else
+      i -= 1
+      count += 1
+    end
+  end
+  return temp
+end
+
+answer = find_duplicate_three([2, 5, 1, 5, 4, 3])
 puts answer
 
-answer = find_duplicate([1, 2, 3, 4, 4, 5, 6, 7, 8])
+answer = find_duplicate_three([1, 2, 3, 4, 4, 5, 6, 7, 8])
 puts answer
