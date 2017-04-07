40263beb5afae7d00bdaef1c734f5bf67cfe832c
maycmlee <may.cm.lee@gmail.com>
2017-04-07 12:22:15 -0400

    add tower of hanoi but does not work

--- stack.rb
+++ stack.rb
@@ -26,12 +26,26 @@ class Stack
       return "Stack is empty. Nothing to pop."
     end
 
-    @tail -= 1
     removed = @stack[@tail]
     @stack[@tail] = nil
+    @tail -= 1
     @count -= 1
     return removed
   end
+
+  def count()
+    return @count
+  end
+
+  def output()
+    i = 0
+    while i < @count
+      print "#{@stack[i]}"
+      i += 1
+    end
+    puts ""
+  end
+
 
 end
 
@@ -40,3 +54,54 @@ s.push(5)
 puts "popped: #{s.pop}"
 
 puts s.pop
+
+# Reverse a stack and put reverse back into same stack. 
+# Only use a stack and temp variable. 
+# Something might be wrong with my stack class push/pop.
+
+def tower_of_hanoi(stack1)
+  puts "stack 1 START:" 
+  puts "#{stack1.output}"
+
+  i = 0
+  stack2 = Stack.new(4)
+
+  while i <= stack1.count
+    temp = stack1.pop
+    puts "stack1 count here: #{stack1.count}"
+    puts "stack 1:" 
+    puts "#{stack1.output}"
+
+    while (stack1.count - i > 0)
+      puts "stack 1:" 
+      puts "#{stack1.output}"
+      stack2.push(stack1.pop)
+      puts "stack 2:" 
+      puts "#{stack2.output}"
+    end
+puts "stack 1 before push temp:" 
+    puts "#{stack1.output}"
+    puts "temp: #{temp}"
+    stack1.push(temp)
+    
+    puts "stack 1 after push temp:" 
+    puts "#{stack1.output}"
+    while (stack2.count != 0)
+      stack1.push(stack2.pop)
+      puts "stack 1:" 
+      puts "#{stack1.output}"
+    end
+
+    i += 1
+  end
+  stack1.push(temp)
+end
+
+stack1 = Stack.new(4)
+stack1.push(4)
+stack1.push(5)
+stack1.push(3)
+stack1.push(2)
+
+
+tower_of_hanoi(stack1)
